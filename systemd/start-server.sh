98e4b3fc40252594dd43a2df813551da46756dda
Phillip Tribble <phillip.tribble@gmail.com>
2016-12-03 22:07:04 -0800

    fix

--- systemd/start-server.sh
+++ systemd/start-server.sh
@@ -2,7 +2,7 @@
 export PICLUSTER_SERVER_PATH="/root/picluster/server"
 export DOCKER="/root/docker"
 export NODE_BIN=`which nodejs`;
-export PORT="9001"
-export AGENTPORT="9002"
+export PORT="3000"
+export AGENTPORT="3002"
 cd $PICLUSTER_SERVER_PATH
 $NODE_BIN server.js
