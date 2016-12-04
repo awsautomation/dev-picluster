4dcfe6cd4a8d9df11a22ed9f90b8b2a166b3ddee
Phillip Tribble <phillip.tribble@gmail.com>
2016-12-04 14:55:54 -0800

    fix

--- server/server.js
+++ server/server.js
@@ -80,7 +80,7 @@ app.get('/nodes', function(req, res){
         } else {
           console.log(response);
           var results = JSON.parse(response.body);
-          addLog('\nRegistered Node:' + node + '\nHostname:' + results.output);
+          addLog('\nRegistered Node:' + results.output);
         }
       })
 
