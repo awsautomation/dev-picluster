49694ddc23d7139f5cff68674c618a073025fe9d
Phillip Tribble <phillip.tribble@gmail.com>
2016-12-03 22:28:25 -0800

    fix

--- server/server.js
+++ server/server.js
@@ -157,7 +157,7 @@ app.get('/start', function(req, res){
         }
 
         request(options, function(error, response, body) {
-          if (response.statusCode != "200") {
+          if (error) {
             res.end("An error has occurred.");
           } else {
             var results = JSON.parse(response.body);
@@ -192,7 +192,7 @@ app.get('/stop', function(req, res){
         }
 
         request(options, function(error, response, body) {
-          if (response.statusCode != "200") {
+          if (error) {
             res.end("An error has occurred.");
           } else {
             var results = JSON.parse(response.body);
