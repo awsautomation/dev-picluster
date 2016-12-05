dd845df3516631c2f0c747b23faeba7b13f8c1db
Phillip Tribble <phillip.tribble@gmail.com>
2016-12-04 22:41:54 -0800

    fix

--- systemd/start-agent.sh
+++ systemd/start-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PICLUSTER_AGENT_PATH="/root/picluster/agent"
 cd $PICLUSTER_AGENT_PATH
-$NODE_BIN agent.js
+nodejs agent.js
--- systemd/start-server.sh
+++ systemd/start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PICLUSTER_SERVER_PATH="/root/picluster/server"
 cd $PICLUSTER_SERVER_PATH
-$NODE_BIN server.js
+nodejs server.js
