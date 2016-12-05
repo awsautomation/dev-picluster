344a01103ab9737f53b19bd66245816afc7982e8
Phillip Tribble <phillip.tribble@gmail.com>
2016-12-04 21:29:44 -0800

    fix

--- web/webconsole.js
+++ web/webconsole.js
@@ -148,6 +148,26 @@ app.get('/status', function(req, res){
   });
 });
 
+app.get('/images', function(req, res){
+  var responseString  = '';
+  var options = {
+    host: server,
+    path: '/images?' + 'token=' + token,
+    port: server_port
+  };
+
+  var request = http.get(options, function(response){
+    response.on('data', function(data) {
+      responseString += data;
+    });
+    response.on('end', function(data){
+      display_log(function(data) {
+        res.end(data);
+      });
+    });
+  });
+});
+
 
 app.get('/hb', function(req, res){
   var responseString  = '';
