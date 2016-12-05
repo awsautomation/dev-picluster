1f1e980acc2a2dd60d7ca8652831c826ac27c8ae
Phillip Tribble <phillip.tribble@gmail.com>
2016-12-04 22:31:43 -0800

    fix

--- systemd/start-agent.sh
+++ systemd/start-agent.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 export PICLUSTER_AGENT_PATH="/root/picluster/agent"
-export NODE_BIN=`which nodejs`;
-export PORT="3001"
-export AGENTPORT="3002"
 cd $PICLUSTER_AGENT_PATH
 $NODE_BIN agent.js
--- systemd/start-server.sh
+++ systemd/start-server.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 export PICLUSTER_SERVER_PATH="/root/picluster/server"
-export DOCKER="/root/docker"
-export NODE_BIN=`which nodejs`;
-export PORT="3000"
-export AGENTPORT="3002"
 cd $PICLUSTER_SERVER_PATH
 $NODE_BIN server.js
