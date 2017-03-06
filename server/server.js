056cb6ba900cbaff93df3181f5bc056a45ce826a
Phillip Tribble <philliptribble@Scalitys-MacBook-Pro.local>
2017-03-06 11:14:39 -0800

    fix

--- server/server.js
+++ server/server.js
@@ -528,29 +528,25 @@ app.get('/changehost', function(req, res) {
                 } else {
                     var build_container = http.get('http://127.0.0.1' + ':' + port + '/build?' + 'token=' + token + '&image=' + container, function(response) {
                         response.on('end', function(data) {
-                            request('http://127.0.0.1' + ':' + port + '/create?' + 'token=' + token + '&container=' + container, function(error, response, body) {
-                                if (!error && response.statusCode == 200) {
+                          var create_container = http.get('http://127.0.0.1' + ':' + port + '/create?' + 'token=' + token + '&container=' + container, function(error, response, body) {
+                              response.on('end', function(data) {
                                     setTimeout(function() {
-                                        request('http://127.0.0.1' + ':' + port + '/stop?' + 'token=' + token + '&container=' + container, function(error, response, body) {
-                                            if (!error && response.statusCode == 200) {
-                                                request('http://127.0.0.1' + ':' + port + '/reloadconfig?' + 'token=' + token, function(error, response, body) {
-                                                    if (!error && response.statusCode == 200) {
-                                                        request('http://127.0.0.1' + ':' + port + '/restart?' + 'token=' + token + '&container=' + container, function(error, response, body) {
-                                                            if (!error && response.statusCode == 200) {
+                                      var stop_container = http.get('http://127.0.0.1' + ':' + port + '/stop?' + 'token=' + token + '&container=' + container, function(error, response, body) {
+                                            response.on('end', function(data) {
+                                              var reload_config = http.get('http://127.0.0.1' + ':' + port + '/reloadconfig?' + 'token=' + token, function(error, response, body) {
+                                                    response.on('end', function(data) {
+                                                          var restart_containers = http.get('http://127.0.0.1' + ':' + port + '/restart?' + 'token=' + token + '&container=' + container, function(error, response, body) {
+                                                            response.on('end', function(data) {
                                                                 res.end('Migrated ' + container + ' from ' + original_host + ' to ' + new_host);
-                                                            };
+                                                            });
                                                         });
-                                                    } else {
-                                                        res.end('\nError connecting with server.');
-                                                    }
+                                                    });
                                                 });
-                                            } else {
-                                                res.end('\nError connecting with server.');
-                                            }
+                                            });
                                         });
 
                                     }, 3000);
-                                };
+                                });
                             });
                         });
                     });
