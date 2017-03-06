15e54ff43ef6e1ca64847996209264b16c7878ea
Phillip Tribble <philliptribble@Scalitys-MacBook-Pro.local>
2017-03-06 14:06:10 -0800

    fix

--- server/server.js
+++ server/server.js
@@ -453,12 +453,12 @@ app.get('/changehost', function(req, res) {
                                 var options = {
                                     uri: 'http://127.0.0.1' + ':' + port + '/stop?' + 'token=' + token + '&container=' + container
                                 };
-                                var stop_container = request.get(options, function(error, response, body) {
-                                    delete config.layout[i][key];
-                                    if (Object.keys(config.layout[i]).length == 1) {
-                                        config.layout.splice(i, 1);
-                                    }
+                                var stop_container = request.get(options, function(error, response, body) {    
                                 });
+                                delete config.layout[i][key];
+                                if (Object.keys(config.layout[i]).length == 1) {
+                                    config.layout.splice(i, 1);
+                                }
                             }
                         }
                     }
