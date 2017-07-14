538bddf1dce75d35737df6054a00f753f5c236dc
Phillip Tribble <phillip.tribble@gmail.com>
2017-07-13 18:13:58 -0700

    Removing

--- docker/mysql/start.sh
+++ docker/mysql/start.sh
@@ -4,14 +4,14 @@ chown -R mysql:mysql /var/lib/mysql
 
 if [ "$(ls -A /var/lib/mysql)" ]; then
    echo "Found existing MySQL Data......"
-   mysqld_safe  --user root&
+   mysqld_safe  --max_connect_errors=10000 --user root&
    sleep infinity
 else
   echo "No existing database found......"
    mysql_install_db --user root
-   mysqld_safe --user root&
+   mysqld_safe --max_connect_errors=10000 --user root&
    sleep 10
    mysqladmin -u root password $sql_password
-   mysql -u root -$sql_password -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '" + $sql_password + "' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+   mysql -u root -p$sql_password -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '" + $sql_password + "' WITH GRANT OPTION; FLUSH PRIVILEGES;"
    sleep infinity
 fi
