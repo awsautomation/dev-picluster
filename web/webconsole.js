6099c60dcda6a9466565decd38cb8de23a1d88cb
TokinRing <support@gnetsolutions.net>
2017-10-07 20:58:04 -0700

    fix directory listing function

--- web/webconsole.js
+++ web/webconsole.js
@@ -63,7 +63,7 @@ function get_directory_list(filepath, extention) {
 
   for (let file in files) {
     if (path.extname(files[file]) === extention) {
-      output = output.append(file);
+      output.push(file);
     }
   }
 
