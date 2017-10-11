b9e2f6da94d014f2b4eb45fe66531afcf3408de1
TokinRing <support@gnetsolutions.net>
2017-10-11 10:00:07 -0700

    fix distLogo switch case statement

--- web/assets/distLogo.js
+++ web/assets/distLogo.js
@@ -5,67 +5,96 @@ function distLogo(dist) {
 		case 'alpine linux':
 			logo.className = "fl-alpine";
 			logo.style.color = "#0d597f";
+			break;
 		case 'centos':
 			logo.className = "fl-centos";
+			break;
 		case 'rhel':
+		  break;
 		case 'rhas':
+		  break;
 		case 'red hat linux':
 			logo.className = "fl-redhat";
 			logo.style.color = "#e93442";
+			break;
 		case 'elementary os':
 			logo.className = "fl-elementary";
+			break;
 		case 'gentoo linux':
 			logo.className = "fl-gentoo";
 			logo.style.color = "#514575";
+			break;
 		case 'mandriva linux':
 			logo.className = "fl-mandriva";
+			break;
 		case 'raspbian':
 			logo.className = "fl-raspberry-pi";
 			logo.style.color = "#d6264f";
+			break;
 		case 'slackware':
 			logo.className = "fl-slackware";
+			break;
 		case 'aosc':
 			logo.className = "fl-aosc";
+			break;
 		case 'coreos':
 			logo.className = "fl-coreos";
+			break;
 		case 'fedora':
 			logo.className = "fl-fedora";
 			logo.style.color = "#294172";
+			break;
 		case 'linux mint':
 			logo.className = "fl-linuxmint";
 			logo.style.color = "#86ce3e";
+			break;
 		case 'manjaro':
 			logo.className = "fl-manjaro";
 			logo.style.color = "#37bf5d";
+			break;
 		case 'apple':
+		  break;
 		case 'osx':
+		  break;
 		case 'macos':
+		  break;
 		case 'darwin':
 			logo.className = "fl-apple";
+			break;
 		case 'nixos':
 			logo.className = "fl-nixos";
+			break;
 		case 'sabayon':
 			logo.className = "fl-sabayon";
+			break;
 		case 'debian':
 			logo.className = "fl-debian";
 			logo.style.color = "#d70751";
+			break;
 		case 'ubuntu':
+		  break;
 		case 'ubuntu linux':
 			logo.className = "fl-ubuntu-inverse";
 			logo.style.color = "#dd4814";
+			break;
 		case 'arch linux':
 			logo.className = "fl-archlinux";
 			logo.style.color = "#1793d1";
+			break;
 		case 'freebsd':
 			logo.className = "fl-freebsd";
 			logo.style.color = "#eb0028";
+			break;
 		case 'mageia':
 			logo.className = "fl-mageia";
+			break;
 		case 'opensuse':
 			logo.className = "fl-opensuse";
 			logo.style.color = "#73ba25";
+			break;
 		default:
-			logo.className = "fl-tux"
+			logo.className = "fl-tux";
+			break;
 	}
 
 	logo.className += " node_status_logo";
