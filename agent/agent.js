dd414fb45a6126a9fcb77035906c669324f13c58
Phillip Tribble <phillip.tribble@gmail.com>
2018-01-20 15:15:57 -0800

    fix

--- agent/agent.js
+++ agent/agent.js
@@ -88,6 +88,7 @@ function monitoring() {
       console.error(err);
     }
     container_stats = stdout.split('\n');
+    console.log(container_stats);
   });
 
   exec('docker ps --format "{{.Status}}"', (err, stdout) => {
